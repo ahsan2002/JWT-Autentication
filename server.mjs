c83bdb9aadd905d1c728be96916d86a69476e0f4
Ahsan <omerjee.ahsan@gmail.com>
2022-12-26 20:06:12 +0500

    done

--- server.mjs
+++ server.mjs
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000', "*"],
+    origin: ['http://localhost:3000', 'https://excited-gold-robe.cyclic.app/',"*"],
     credentials: true
 }));
 
